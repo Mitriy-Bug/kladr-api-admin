895a9479a855043c201e6ecbf6ef8bd62410dd4e
Mitriy_Bug <140303168+Mitriy-Bug@users.noreply.github.com>
2023-11-06 21:01:01 +0300

    Add files via upload

---
+++ kladr.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package    Plg_Content_Updaterating
+ * @author     Dmitry Denisov <info@codersite.ru>
+ * @copyright  (C) 2023 codersite.ru. All rights reserved.
+ * @license    GNU General Public License version 3 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+/**
+ * Base plugin class.
+ *
+ * @since  1.0
+ */
+class PlgContentKladr extends CMSPlugin
+{
+
+	public function onContentPrepareData($context, $data)
+	{
+		
+		if ($context === 'com_content.article') {
+			if ($data->catid === 23) {
+				echo "
+				<link href='/kladr/jquery.fias.min.css' rel='stylesheet'>
+				<script src='https://yastatic.net/jquery/3.3.1/jquery.min.js' type='text/javascript'></script>
+				<script src='/kladr/jquery.fias.min.js' type='text/javascript'></script>
+				<script src='/kladr/script-admin.js' type='text/javascript'></script>";
+
+				//JFactory::getApplication()->enqueueMessage('ID автосалона - ');
+			}
+		} 
+	}
+}
